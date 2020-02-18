be4f921b4c9524f8693c4d1bca4f45de35e94732
Samuel <samuel.d.flis@gmail.com>
2020-02-18 15:35:11 +0100

    Add RPath fix for custom install locations and add more tests

--- pytest/test_icffile.py
+++ pytest/test_icffile.py
@@ -9,7 +9,10 @@ def icf_impl(request):
 
 
 def test_write_and_readback_from_icffile(icf_impl):
-    os.remove("/tmp/test.icf")
+    try:
+        os.remove("/tmp/test.icf")
+    except:
+        pass
     f = icf_impl("/tmp/test.icf")
     testdata1 = b'blablakaskdlaskd'
     testdata2 = b'blabasdasdlakaskdlaskd'
@@ -26,7 +29,10 @@ def test_write_and_readback_from_icffile(icf_impl):
     assert f.read_at(1) == testdata2, "Read back correct data"
 
 def test_read_while_writing(icf_impl):
-    os.remove("/tmp/test.icf")
+    try:
+        os.remove("/tmp/test.icf")
+    except:
+        pass
     f = icf_impl("/tmp/test.icf")
     testdata1 = b'blablakaskdlaskd'
     testdata2 = b'blabasdasdlakaskdlaskd'
@@ -37,7 +43,10 @@ def test_read_while_writing(icf_impl):
 
 
 def test_write_read_multiple_bunches(icf_impl):
-    os.remove("/tmp/test.icf")
+    try:
+        os.remove("/tmp/test.icf")
+    except:
+        pass
     f = pyicf.ICFFile("/tmp/test.icf",bunchsize=50)
     data = b"0"*26
 
@@ -50,6 +59,28 @@ def test_write_read_multiple_bunches(icf_impl):
 
     for i in range(6):
         assert f.read_at(i) == data
+
+def test_read_merged_files(icf_impl):
+    try:
+        os.remove("/tmp/test1.icf")
+        os.remove("/tmp/test2.icf")
+        os.remove("/tmp/cat.icf")
+    except:
+        pass
+    f1 = icf_impl("/tmp/test1.icf")
+    f2 = icf_impl("/tmp/test2.icf")
+    testdata1 = b'blablakaskdlaskd'
+    testdata2 = b'blabasdasdlakaskdlaskd'
+    f1.write(testdata1)
+    f2.write(testdata2)
+    f1.close()
+    f2.close()
+    os.system("cat /tmp/test1.icf /tmp/test2.icf >> /tmp/cat.icf")
+    fcat = icf_impl("/tmp/cat.icf")
+
+    assert fcat.size() == 2, "Correct number of elements"
+    assert fcat.read_at(0) == testdata1, "Correct data from file 1"
+    assert fcat.read_at(1) == testdata2, "Correct data from file 2"
 
 
 
@@ -65,3 +96,6 @@ def test_bunch_buffer():
     bf[n] = [n]
     assert n in bf, "New element in Bunch Buffer"
     assert 0 not in bf, "Oldest element removed from Bunch Buffer"
+
+
+
