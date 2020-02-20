eaaff188e104ea434e30645b71c1c842e4d23d2d
Samuel <samuel.d.flis@gmail.com>
2020-02-20 23:26:37 +0100

    CPP version of ICF now basically writes according to the specification implemented by the python version

--- include/icf/icfFile.h
+++ include/icf/icfFile.h
@@ -27,8 +27,7 @@
 namespace icf{
 
 class ICFFile{
-public:
-    struct ICFFileHeader{
+        struct ICFFileHeader{
 
         ICFFileHeader():version_(0),compression_(0),unused_(0),ext_head_len_(0){
             strncpy(file_identifier_,"ICF",4);
@@ -62,54 +61,30 @@ public:
         uint16_t ext_head_len_;
     };
 
-
-    enum access_mode{read, trunc, append};
-
-    ICFFile(std::string path, ICFFile::access_mode mode=ICFFile::append);
-
-    ~ICFFile();
-
-    void write(const void* data, std::size_t size);
-
-    std::shared_ptr<std::vector<unsigned char> > read_at(uint64_t index);
-
-    void flush();
-
-    void close(){file_handle_.close(); }
-
-    uint64_t size(){return object_index_.size();}
-
-    std::chrono::time_point<std::chrono::system_clock> get_timestamp(){return file_header_.time_stamp2_;}
-private:
-
-    void scan_file(uint64_t pos);
-
-    ICFFileHeader file_header_;
-    std::fstream file_handle_;
-    uint64_t current_read_pointer_;
-    uint64_t current_write_pointer_;
-    std::vector<uint64_t> object_index_;
-    Archive<std::fstream> serializer_stream_;
-};
-
-
-
-
-class ICFFileV2{
     struct ICFBunchTrailer{
 
-        ICFBunchTrailer(std::weak_ptr<std::vector<char*> >):version_(0){
+        ICFBunchTrailer(uint64_t file_offset,
+        uint64_t prev_bunch_offset,
+        uint64_t bunch_data_offset,
+        uint32_t n_chunks_in_bunch,
+        uint32_t bunch_number):file_offset_(file_offset),
+                                prev_bunch_offset_(prev_bunch_offset),
+                                bunch_data_offset_(bunch_data_offset),
+                                n_chunks_in_bunch_(n_chunks_in_bunch),
+                                bunch_number_(bunch_number),
+                                version_(0){
             time_stamp2_ = std::chrono::system_clock::now();
             time_stamp_ = std::chrono::system_clock::to_time_t(time_stamp2_);
         }
 
-        void Serialize(Archive<std::fstream>& archive)
+        template <class T>
+        void Serialize(T& archive)
         {
             archive & version_;
 
 
             archive & unused_ &
-                    time_stamp_ & 
+                    time_stamp_ &
                     file_offset_ &
                     prev_bunch_offset_ &
                     bunch_data_offset_ &
@@ -118,7 +93,7 @@ class ICFFileV2{
                     flags_;
 
         }
-   
+
         uint16_t version_;
         uint16_t unused_;
         std::time_t time_stamp_;
@@ -130,16 +105,16 @@ class ICFFileV2{
         uint32_t bunch_number_;
         uint32_t flags_;
 
-        uint32_t trailer_start_offset_;
+        // uint32_t trailer_start_offset_;
 
     };
 
 public:
     enum access_mode{read, trunc, append};
 
-    ICFFileV2(std::string path, ICFFileV2::access_mode mode=ICFFileV2::append);
+    ICFFile(std::string path, ICFFile::access_mode mode=ICFFile::append, uint32_t bunchsize = 1000000);
 
-    ~ICFFileV2();
+    ~ICFFile();
 
     void write(const void* data, std::size_t size);
 
@@ -147,11 +122,13 @@ public:
 
     void flush();
 
-    void close(){file_handle_.close(); }
+    void close(){flush();file_handle_.close(); }
 
     uint64_t size(){return object_index_.size();}
 
     std::chrono::time_point<std::chrono::system_clock> get_timestamp(){return file_header_.time_stamp2_;}
+
+
 private:
 
     void scan_file(uint64_t pos);
@@ -162,7 +139,12 @@ private:
     uint64_t current_write_pointer_;
     std::vector<uint64_t> object_index_;
     Archive<std::fstream> serializer_stream_;
-    std::vector<std::unique_ptr<char*> > write_buffer_;
+    std::vector<std::shared_ptr<std::vector<char> > > write_buffer_;
+    uint32_t n_entries_;
+    uint32_t cbunchoffset_;
+    uint32_t bunchsize_;
+    uint64_t last_bunch_fp_;
+    uint32_t bunch_number_;
     // std::vector<std::unique_prt<char*> > read_buffer_;
 };
 
--- src/icfFile.cc
+++ src/icfFile.cc
@@ -1,11 +1,21 @@
 #include "icf/icfFile.h"
 #include <fstream>
 #include <iostream>
-
+#include <array>
 
 using namespace icf;
 
-ICFFile::ICFFile(std::string path, ICFFile::access_mode mode):serializer_stream_(file_handle_){
+
+
+
+
+ICFFile::ICFFile(std::string path, ICFFile::access_mode mode, uint32_t bunchsize):
+            serializer_stream_(file_handle_),
+            n_entries_(0),
+            cbunchoffset_(0),
+            bunchsize_(bunchsize),
+            last_bunch_fp_(0),
+            bunch_number_(0){
     auto omode = std::ios_base::in | std::ios_base::binary;
     switch(mode){
         case append:
@@ -27,83 +37,7 @@ ICFFile::ICFFile(std::string path, ICFFile::access_mode mode):serializer_stream_
 
     if(current_write_pointer_>current_read_pointer_ && mode !=trunc){
         serializer_stream_>>file_header_;
-        scan_file(file_handle_.tellp());        
-    }
-    else{
-        serializer_stream_<<file_header_;
-        current_write_pointer_ = file_handle_.tellp();
-    }
-
-}
-
-ICFFile::~ICFFile(){
-    file_handle_.close();
-}
-
-
-
-void ICFFile::write(const void* data, std::size_t size){
-    file_handle_.seekp(current_write_pointer_);
-    object_index_.push_back(file_handle_.tellp());
-    serializer_stream_<<size;
-    file_handle_.write((const char*) data, size);
-    current_write_pointer_ = file_handle_.tellp();
-}
-
-
-
-std::shared_ptr<std::vector<unsigned char> > ICFFile::read_at(uint64_t index){
-    
-    auto fp = object_index_.at(index);
-    size_t obj_size;
-    file_handle_.seekp(fp);
-    serializer_stream_>>obj_size;
-    auto data = std::make_shared<std::vector<unsigned char> >(obj_size);
-
-    file_handle_.read((char*) data->data(), obj_size);
-    return data;
-}
-
-
-void ICFFile::scan_file(uint64_t pos){
-    size_t obj_size;
-    auto bheader_size = sizeof(obj_size);
-    auto length = current_write_pointer_;
-    file_handle_.seekp(pos);
-    while(file_handle_.tellp()<length){
-        object_index_.push_back(file_handle_.tellp());
-        serializer_stream_>>obj_size;
-        pos += obj_size + bheader_size;
-        file_handle_.seekp(pos);
-    }
-}
-
-
-
-
-ICFFileV2::ICFFileV2(std::string path, ICFFileV2::access_mode mode):serializer_stream_(file_handle_){
-    auto omode = std::ios_base::in | std::ios_base::binary;
-    switch(mode){
-        case append:
-            omode |= std::ios_base::app | std::ios_base::out;
-            break;
-        case trunc:
-            omode |= std::ios_base::trunc | std::ios_base::out;
-        default:
-            break;
-    }
-
-    file_handle_.open(path, omode);
-
-    current_read_pointer_ = file_handle_.tellg();
-    file_handle_.seekp(0, std::ios_base::end);
-    current_write_pointer_ = file_handle_.tellp();
-    auto length = current_write_pointer_;
-    file_handle_.seekp(0);
-
-    if(current_write_pointer_>current_read_pointer_ && mode !=trunc){
-        serializer_stream_>>file_header_;
-        scan_file(file_handle_.tellp());        
+        scan_file(file_handle_.tellp());
     }
     else{
         serializer_stream_<<file_header_;
@@ -113,20 +47,70 @@ ICFFileV2::ICFFileV2(std::string path, ICFFileV2::access_mode mode):serializer_s
     // serializer_stream_>>bt;
 }
 
-ICFFileV2::~ICFFileV2(){
+ICFFile::~ICFFile(){
     file_handle_.close();
 }
 
 
-void ICFFileV2::scan_file(uint64_t pos){
-    size_t obj_size;
-    auto bheader_size = sizeof(obj_size);
-    auto length = current_write_pointer_;
-    file_handle_.seekp(pos);
-    while(file_handle_.tellp()<length){
-        object_index_.push_back(file_handle_.tellp());
-        serializer_stream_>>obj_size;
-        pos += obj_size + bheader_size;
-        file_handle_.seekp(pos);
+void ICFFile::scan_file(uint64_t pos){
+    // size_t obj_size;
+    // auto bheader_size = sizeof(obj_size);
+    // auto length = current_write_pointer_;
+    // file_handle_.seekp(pos);
+    // while(file_handle_.tellp()<length){
+    //     object_index_.push_back(file_handle_.tellp());
+    //     serializer_stream_>>obj_size;
+    //     pos += obj_size + bheader_size;
+    //     file_handle_.seekp(pos);
+    // }
+}
+
+void ICFFile::write(const void* data, std::size_t size){
+    auto datacopy = std::make_shared<std::vector<char> >(std::vector<char>(size));
+    memcpy(datacopy->data(),data, size);
+    write_buffer_.push_back(datacopy);
+    n_entries_ +=1;
+    cbunchoffset_ += size;
+    if(cbunchoffset_> bunchsize_){
+        flush();
     }
+
+}
+
+void ICFFile::flush(){
+    cbunchoffset_ = 0;
+    if(write_buffer_.size()<1){
+        return;
+    }
+    file_handle_.seekp(0, std::ios_base::end);
+    auto bunch_start_fp = file_handle_.tellp();
+    std::vector<uint32_t> cbunchindex;
+    // cbunchindex_
+    for(auto &data: write_buffer_){
+        cbunchindex.push_back(data->size());
+        file_handle_.write(data->data(),data->size());
+    }
+    auto curr_bt_fp = file_handle_.tellp();
+    ICFFile::ICFBunchTrailer bunch_trailer(curr_bt_fp,
+                                           curr_bt_fp-last_bunch_fp_,
+                                           curr_bt_fp-bunch_start_fp,
+                                           write_buffer_.size(),
+                                           bunch_number_);
+    serializer_stream_<<bunch_trailer;
+    for(const auto &index: cbunchindex){
+        serializer_stream_<<index;
+    }
+    uint32_t offset_to_bt_start = file_handle_.tellp() - curr_bt_fp;
+    serializer_stream_<<offset_to_bt_start;
+    last_bunch_fp_ = curr_bt_fp;
+    write_buffer_.clear();
+    cbunchoffset_ = 0;
+    bunch_number_++;
+
+}
+
+
+std::shared_ptr<std::vector<unsigned char> > ICFFile::read_at(uint64_t index){
+
+
 }
